97f6a912aa9a269ee85b5d30861d095d56e9775c
keelia <xiaoyuetang2017@gmail.com>
2019-07-29 15:38:31 +0900

    ---db auth---

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const PORT = process.env.PORT || 4000;
 let Todo = require("./todo.model");
 app.use(cors());
 app.use(bodyParser.json());
-mongoose.connect("mongodb://127.0.0.1:27017/todos", { useNewUrlParser: true });
+mongoose.connect("mongodb://admin:myadminpassword@192.168.0.100:27017/todos", {
+  useNewUrlParser: true
+});
 const connection = mongoose.connection;
 
 connection.once("open", function() {
